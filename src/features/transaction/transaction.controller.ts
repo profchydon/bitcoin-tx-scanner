4a5a59dcdc9bef63c35dcf0d01b9a6c2f824e8fb
Chidi Nkwocha <chidi.nkwocha@pandatron.ai>
2022-04-28 01:47:55 +0100

    Fetch associated transaction data for an OP_RETURN value

--- src/features/transaction/transaction.controller.ts
+++ src/features/transaction/transaction.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import { ResponseInterceptor } from 'src/common/interceptors/response-transform.interceptor';
 import { RpcService } from '../../common/rpc/rpc.service';
+import { GetTransactionHandler } from './handlers/get-transaction.handler';
 
 @Controller('opreturn')
 export class TransactionController {
-  constructor(private readonly rpcService: RpcService) {}
+  constructor(
+    private readonly rpcService: RpcService,
+    private getTransaction: GetTransactionHandler,
+  ) {}
 
   @Get('sync')
   async triggerInitialSync() {
     try {
       const response = await this.rpcService.initialSync();
+    } catch (error) {}
+  }
+
+  @Get(':opReturnData')
+  @UseInterceptors(ResponseInterceptor)
+  async getTransactions(@Param('opReturnData') opReturnData: string) {
+    try {
+      const transactions = await this.getTransaction.handle(opReturnData);
+      return {
+        message: 'Success',
+        response: transactions,
+      };
     } catch (error) {}
   }
 }
--- src/features/transaction/transaction.module.ts
+++ src/features/transaction/transaction.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 import { RpcModule } from 'src/common/rpc/rpc.module';
+import { GetTransactionHandler } from './handlers/get-transaction.handler';
 import { TransactionController } from './transaction.controller';
 
 @Module({
   imports: [RpcModule],
   controllers: [TransactionController],
-  providers: [],
+  providers: [PrismaService, Logger, GetTransactionHandler],
   exports: [],
 })
 export class TransactionModule {}
